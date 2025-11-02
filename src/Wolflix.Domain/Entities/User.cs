23e09ad8db45d226b4755929871c5ab70a7a3ca2
João - Windows <contato.jsbarbosa@gmail.com>
2025-11-01 22:26:45 -0300

    :white_check_mark: test(domain): cria testes unitários para validar regras de negócio da entidade User

---
+++ src/Wolflix.Domain/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolflix.Domain.Exceptions;
+
+namespace Wolflix.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public User(string name, string email) { 
+        
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            valid();
+        }
+
+        private void valid()
+        {
+            if (String.IsNullOrWhiteSpace(Name)) throw new EntityValidRequest("O Nome é obrigatório");
+            if (String.IsNullOrWhiteSpace(Email)) throw new EntityValidRequest("O E-mail é obrigatório");
+        }
+    }
+}
---
+++ tests/Wolflix.Tests/Domain/Entity/UserUnitTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wolflix.Domain.Entities;
+using Wolflix.Domain.Exceptions;
+
+namespace Wolflix.Tests.Domain.Entity
+{
+    public class UserUnitTest
+    {
+
+        [Fact(DisplayName = nameof(ShouldNotBeNullAfterSaving))]
+        [Trait("User","Aggregation")]
+        public void ShouldNotBeNullAfterSaving()
+        {
+            var initialData = new
+            {
+                Name = "João",
+                Email = "contato@joao.com"
+            };
+            var user = new User(initialData.Name, initialData.Email);
+
+            Assert.NotNull(user);
+            Assert.Equal(initialData.Name, user.Name);
+            Assert.Equal(initialData.Email, user.Email);
+        }
+
+        [Fact(DisplayName = nameof(IDMustNotBeNullAfterCreation))]
+        [Trait("User","Aggregation")]
+        public void IDMustNotBeNullAfterCreation()
+        {
+            var initialData = new
+            {
+                Name = "Lucas",
+                Email = "contato@lucas.com"
+            };
+
+            var user = new User(initialData.Name, initialData.Email);
+            Assert.NotEqual(default(Guid), user.Id);
+        }
+
+        [Theory(DisplayName =nameof(ShouldThrowExceptionWhenTryingSaveUserWithEmptyName))]
+        [Trait("User","Aggregation")]
+        [InlineData("")]
+        [InlineData("  ")]
+        [InlineData(null)]
+        public void ShouldThrowExceptionWhenTryingSaveUserWithEmptyName(string? name)
+        {
+
+     
+            var email = "teste@teste";
+
+            Action action = () => new User(name, email);
+            var exception = Assert.Throws<EntityValidRequest>(action);
+            Assert.Equal("O Nome é obrigatório", exception.Message);
+
+        }
+
+        [Theory(DisplayName = nameof(ShouldThrowExceptionWhentryingSaveUserWithEmptyEmail))]
+        [Trait("User","Aggregation")]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void ShouldThrowExceptionWhentryingSaveUserWithEmptyEmail(string? email)
+        {
+            var name = "Jonas";
+            Action action = () => new User(name, email);
+            var exception = Assert.Throws<EntityValidRequest>(action);
+            Assert.Equal("O E-mail é obrigatório", exception.Message);
+        }
+    }
+}
